92f68168d6094340127d054b6b539fdca90224b9
leciric <lecirics@gmail.com>
2020-01-18 02:05:28 -0300

    Mobile implementado

---
+++ backend/src/Controllers/DevController.js
@@ -0,0 +1,82 @@
+const axios = require('axios');
+const Dev = require('../models/dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+
+async index (request, response){
+    const devs = await Dev.find();
+    return response.json(devs);
+},
+
+
+async store (request, response) {
+    const { github_username, techs, latitude, longitude} = request.body;
+
+    let dev = await Dev.findOne({ github_username});
+
+    if(!dev){
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+        const { name = login, avatar_url, bio } = apiResponse.data;
+    
+        console.log(name, avatar_url, bio, github_username);
+    
+        const techsArray = parseStringAsArray(techs);
+        
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+        };
+    
+        dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location,
+        });
+    
+    }
+
+    
+    return response.json(dev);
+},
+
+async update(request, response) {
+    const {github_username,name, bio, techs, location} = request.body;
+
+    let dev = await Dev.findOne({github_username});
+
+    if(!dev)
+    {
+        const techsArray = parseStringAsArray(techs);
+        
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+        };
+
+        dev = await Dev.update({
+            name,
+            bio,
+            techs: techsArray,
+            location: location,
+        })
+    }
+    return response.json(dev);
+
+},
+
+async destroy(request, response) {
+    const {user} = request.params;
+    console.log(user);
+
+    Dev.deleteOne({github_username: user}, (err) => {
+        if (err) return handleError(err);
+    } );
+    return response.json(Dev);
+},
+
+};
---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+import {StyleSheet} from 'react-native';
+
+
+function Profile({navigation}){
+    const githubUsername = navigation.getParam('github_username');
+    return <WebView style={styles.webview} source={{ uri: `https://github.com/${githubUsername}` }}/>
+}
+
+const styles = StyleSheet.create ({
+    webview: {
+        flex: 1,
+    }
+})
+
+
+export default Profile;
+
